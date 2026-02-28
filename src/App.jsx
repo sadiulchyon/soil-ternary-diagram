9bf003b11f1ac38f8904fb0d555b34cf7a6972e5
sadiulchyon <chyon.msa@gmail.com>
2026-02-28 05:37:21 -0600

    Use top-level soil-ternary-diagram component file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SoilTernaryDiagram from '../soil-ternary-diagram.jsx';
+
+function App() {
+  return (
+    <main className="app-shell">
+      <SoilTernaryDiagram />
+    </main>
+  );
+}
+
+export default App;
